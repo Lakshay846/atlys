58ed36b1d33d013f688e9ba1d9711c3a467564e9
Lakshay Kumar <lakshkumar846@gmail.com>
2025-10-25 14:33:59 +0530

    added hashrouting for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Feed from "./pages/Feed";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
-        <Route path="/atlys/" element={<Feed />} />
-        <Route path="/atlys/signin" element={<SignIn />} />
-        <Route path="/atlys/signup" element={<SignUp />} />
+        <Route path="/" element={<Feed />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
-    </BrowserRouter>
   );
 }
 
